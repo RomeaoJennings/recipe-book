9d85db8ba9745feb42cceb5a068ed31893326677
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-03 23:28:44 -0800

    Add hacked navigation using header events

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,7 +9,8 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Okra', 10)
   ];
   constructor() { }
 
