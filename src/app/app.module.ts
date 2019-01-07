33afd81c1d82137823243b2c156a06dabc5662c8
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-07 12:18:17 -0800

    Refactor auth and recipe lists to individual modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
-import { RecipesRoutingModule } from './recipes/recipes-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RecipesRoutingModule,
-    AppRoutingModule,
     HttpModule,
+    AppRoutingModule,
     RecipesModule,
-    SharedModule
+    SharedModule,
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [
     ShoppingListService,
